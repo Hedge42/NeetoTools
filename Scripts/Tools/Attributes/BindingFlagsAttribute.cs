5358d36a24db4d64db63605bd66161e60d207036
Hedge42 <30989650+Hedge42@users.noreply.github.com>
2024-10-03 01:49:35 -0600

    remove extras

---
+++ Scripts/Tools/Attributes/BindingFlagsAttribute.cs
@@ -0,0 +1,10 @@
+﻿//using Cysharp.Threading.Tasks;
+using System;
+using System.Reflection;
+
+[AttributeUsage(AttributeTargets.Field | AttributeTargets.Class)]
+public class BindingFlagsAttribute : Attribute
+{
+    public BindingFlags flags { get; set; }
+    public BindingFlagsAttribute(BindingFlags flags) => this.flags = flags;
+}
