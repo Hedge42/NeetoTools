d85c28ba2acbad6bea4cfc57a2ae10f31a05f46f
Hedge42 <30989650+Hedge42@users.noreply.github.com>
2024-10-08 22:29:57 -0600

    battlefieldwork

---
+++ Scripts/Animation/AnimationSystem.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Neeto
+{
+    public class AnimationSystem : MonoBehaviour
+    {
+        // Start is called before the first frame update
+        void Start()
+        {
+        
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+        
+        }
+    }
+}
